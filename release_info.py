fd8384dab795e9a1b7c204f7397e7130853040c7
berkekarabulut19-collab <berkekarabulut19@gmail.com>
2026-08-13 15:29:47 +0300

    Bump Oranix release to v18001

--- release_info.py
+++ release_info.py
@@ -1,5 +1,5 @@
 """Single source of truth for the shipped Oranix build."""
 
-APP_VERSION = "18000.0"
-APP_DISPLAY_VERSION = "v18000"
+APP_VERSION = "18001.0"
+APP_DISPLAY_VERSION = "v18001"
 UPDATE_MANIFEST_ENV = "ORANIX_UPDATE_MANIFEST_URL"
